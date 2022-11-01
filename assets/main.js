76ecbf6d34c9ea3de39fb707b6f19c8c50fffe3c
simla04 <jessicasowi@gmail.com>
2022-11-01 19:45:06 +0100

    .

---
+++ assets/main.js
@@ -0,0 +1,39 @@
+const griglia = document.getElementById('griglia');
+// console.log( griglia );
+
+
+//creiamo una funzione per creare un div quadrato dentro la griglia
+
+
+function creazioneQuadrato(){
+    const div = document.createElement('div');
+    div.classList.add('quadrato');
+    return div;
+}
+
+// console.log( creazioneQuadrato() );
+
+
+
+// inserimento il quadrato creato dalla funzione nella griglia
+// griglia.append( creazioneQuadrato() );
+
+
+
+//64 quadrati
+for( let i = 0; i < 64; i++ ){ 
+    let elementoCorrente = creazioneQuadrato();
+    console.log(elementoCorrente);
+    
+
+    var count = 1;
+    elementoCorrente.addEventListener('click', function(){
+        console.log(this)
+        
+        this.classList.toggle('active');
+    count++;
+
+    })
+    
+    griglia.append( elementoCorrente );
+}
